9516bf69b7e75b7bdc0c82b72c7e551589dcbdc6
kirupanandhini <107332841+kirupanandhini@users.noreply.github.com>
2022-06-15 18:49:25 -0700

    uploading the sampleapk

---
+++ 3rdKibo-RPC_SampleAPK (1)/SampleApk/app/src/main/java/jp/jaxa/iss/kibo/rpc/sampleapk/YourService.java
@@ -0,0 +1,83 @@
+package jp.jaxa.iss.kibo.rpc.sampleapk;
+
+import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
+
+import gov.nasa.arc.astrobee.Result;
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
+
+import org.opencv.core.Mat;
+
+/**
+ * Class meant to handle commands from the Ground Data System and execute them in Astrobee
+ */
+
+public class YourService extends KiboRpcService {
+    @Override
+    protected void runPlan1(){
+        // the mission starts
+        api.startMission();
+
+        // move to a point
+        Point point = new Point(10.71f, -7.5f, 4.48f);
+        Quaternion quaternion = new Quaternion(0f, 0f, 0f, 1f);
+        api.moveTo(point, quaternion, false);
+
+        // report point1 arrival
+        api.reportPoint1Arrival();
+
+        // get a camera image
+        Mat image = api.getMatNavCam();
+
+        // irradiate the laser
+        api.laserControl(true);
+
+        // take target1 snapshots
+        api.takeTarget1Snapshot();
+
+        // turn the laser off
+        api.laserControl(false);
+
+        /* ******************************************** */
+        /* write your own code and repair the air leak! */
+        /* ******************************************** */
+
+        // send mission completion
+        api.reportMissionCompletion();
+    }
+
+    @Override
+    protected void runPlan2(){
+        // write here your plan 2
+    }
+
+    @Override
+    protected void runPlan3(){
+        // write here your plan 3
+    }
+
+    // You can add your method
+    private void moveToWrapper(double pos_x, double pos_y, double pos_z,
+                               double qua_x, double qua_y, double qua_z,
+                               double qua_w){
+
+        final Point point = new Point(pos_x, pos_y, pos_z);
+        final Quaternion quaternion = new Quaternion((float)qua_x, (float)qua_y,
+                                                     (float)qua_z, (float)qua_w);
+
+        api.moveTo(point, quaternion, true);
+    }
+
+    private void relativeMoveToWrapper(double pos_x, double pos_y, double pos_z,
+                               double qua_x, double qua_y, double qua_z,
+                               double qua_w) {
+
+        final Point point = new Point(pos_x, pos_y, pos_z);
+        final Quaternion quaternion = new Quaternion((float) qua_x, (float) qua_y,
+                (float) qua_z, (float) qua_w);
+
+        api.relativeMoveTo(point, quaternion, true);
+    }
+
+}
+
